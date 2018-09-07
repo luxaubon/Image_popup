47248020d1454c2c2328dc2662c252275cd3fc39
lux aubon <luxaubon@hotmail.com>
2018-09-07 16:20:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<script type="text/javascript" src="jquery-1.9.1.min.js"></script>
+  <link href="magnific-popup.css" rel="stylesheet" type="text/css">
+    
+	<script>	
+	$(document).ready(function(){
+		
+		 $('.image-link').magnificPopup({type:'image'});
+	});
+	</script>
+	<a href="poppu.png" class="image-link" >
+		<div style="background-image:url('3.jpg'); background-position:center center; background-size:cover;height:250px;width:250px" >
+		<img src="poppu.png" >
+	</a>
+											
+    <script src="jquery.magnific-popup.min.js" type="text/javascript"></script>
+
